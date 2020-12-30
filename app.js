e3d156c8e12d0a04e9c247c3395e69c0671227ce
Ikraam <IbnDaanis@Gmail.com>
2020-12-30 09:15:54 +0000

    Put the results into the display

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+const playerScoreDisplay = document.querySelector('.player span')
+const computerScoreDisplay = document.querySelector('.computer span')
+const resultDisplay = document.querySelector('#resultDisplay')
+
 const options = ['rock', 'paper', 'scissors']
+let playerScore = 0
+let computerScore = 0
 
 const computerPlay = () => {
   return options[Math.floor(Math.random() * 3)]
@@ -8,44 +14,43 @@ const playRound = (playerSelection, computerSelection) => {
   const player = options.indexOf(playerSelection)
   const computer = options.indexOf(computerSelection)
   if (player === 0 && computer === 1) {
-    return 'You lose! Paper beats Rock'
+    return 'You lost! Paper beats Rock'
   } else if (player === 0 && computer === 2) {
-    return 'You win! Rock beats Scissors'
+    return 'You won! Rock beats Scissors'
   } else if (player === 1 && computer === 0) {
-    return 'You win! Paper beats Rock'
+    return 'You won! Paper beats Rock'
   } else if (player === 1 && computer === 2) {
-    return 'You lose! Scissors beats Paper'
+    return 'You lost! Scissors beats Paper'
   } else if (player === 2 && computer === 0) {
-    return 'You lose! Rock beats Scissors'
+    return 'You lost! Rock beats Scissors'
   } else if (player === 2 && computer === 1) {
-    return 'You win! Scissors beats Paper'
+    return 'You won! Scissors beats Paper'
   } else {
     return 'Tie! No one wins'
   }
 }
 
-const game = () => {
-  let playerScore = 0
-  let computerScore = 0
-
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase()
-    const result = playRound(playerSelection, computerPlay())
-    if (result.includes('lose')) {
-      computerScore++
-    } else if (result.includes('won')) {
-      playerScore++
-    }
-
-    console.log(
-      `${result}, your score is ${playerScore}, the computer's score is ${computerScore}.`
-    )
+const game = playerSelection => {
+  const result = playRound(playerSelection, computerPlay())
+  if (result.includes('lost')) {
+    computerScore++
+  } else if (result.includes('won')) {
+    playerScore++
   }
+
+  playerScoreDisplay.textContent = playerScore
+  computerScoreDisplay.textContent = computerScore
+  resultDisplay.textContent = result
+
+  console.log(
+    `${result}, your score is ${playerScore}, the computer's score is ${computerScore}.`
+  )
 }
 
-// const buttons = document.querySelectorAll('button')
-// buttons.forEach(button => {
-//   button.onclick(() => console.log(button.dataset.choice))
-// })
-
-// console.log([...buttons].map(button => button.dataset.choice))
+const buttons = [...document.querySelectorAll('button')]
+buttons.forEach(button =>
+  button.addEventListener('click', e => {
+    const playerSelection = e.target.dataset.choice
+    game(playerSelection)
+  })
+)
