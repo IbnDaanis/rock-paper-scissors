c806f9af38583801edd1b018a5e4d5cc0f0e71c8
Ikraam <IbnDaanis@Gmail.com>
2020-12-30 16:10:51 +0000

    Added max score of 5, changed styling, added a reset

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,18 @@
 const playerScoreDisplay = document.querySelector('.player span')
 const computerScoreDisplay = document.querySelector('.computer span')
 const resultDisplay = document.querySelector('#resultDisplay')
+const buttons = [...document.querySelectorAll('.choices button')]
+const resetButton = document.querySelector('#reset')
 
 let playerScore = 0
 let computerScore = 0
+
+const disableButtons = trueOrFalse => {
+  buttons.forEach(button => {
+    button.disabled = trueOrFalse
+    button.classList.toggle('disabled')
+  })
+}
 
 const game = playerSelection => {
   const result = playRound(playerSelection, computerPlay())
@@ -12,12 +21,27 @@ const game = playerSelection => {
   } else if (result.includes('won')) {
     playerScore++
   }
+
   playerScoreDisplay.textContent = playerScore
   computerScoreDisplay.textContent = computerScore
   resultDisplay.textContent = result
+
+  if (playerScore === 5) {
+    resultDisplay.textContent = 'You reached 5 points and won the game!'
+    resultDisplay.classList.add('winner')
+    disableButtons(true)
+    resetButton.style.visibility = 'visible'
+    resetButton.style.opacity = 1
+  } else if (computerScore === 5) {
+    resultDisplay.textContent =
+      'You lost, the computer reached 5 points and won the game!'
+    resultDisplay.classList.add('loser')
+    disableButtons(true)
+    resetButton.style.visibility = 'visible'
+    resetButton.style.opacity = 1
+  }
 }
 
-const buttons = [...document.querySelectorAll('button')]
 buttons.forEach(button =>
   button.addEventListener('click', e => {
     const playerSelection = e.target.dataset.choice
@@ -25,6 +49,19 @@ buttons.forEach(button =>
   })
 )
 
-setTimeout(() => {
-  document.querySelector('.lol').style.transform = 'translateX(200%)'
-}, 10000)
+const reset = () => {
+  playerScore = 0
+  computerScore = 0
+  resultDisplay.textContent = 'Start the game!'
+  resultDisplay.classList.remove('winner', 'loser')
+  disableButtons(false)
+
+  playerScoreDisplay.textContent = playerScore
+  computerScoreDisplay.textContent = computerScore
+
+  resetButton.style.visibility = 'hidden'
+  resetButton.style.opacity = 0
+}
+resetButton.addEventListener('click', () => {
+  reset()
+})
--- scripts/gameRound.js
+++ scripts/gameRound.js
@@ -8,17 +8,17 @@ const playRound = (playerSelection, computerSelection) => {
   const player = options.indexOf(playerSelection)
   const computer = options.indexOf(computerSelection)
   if (player === 0 && computer === 1) {
-    return 'You lost! Paper beats Rock'
+    return 'You lost the round! Paper beats Rock'
   } else if (player === 0 && computer === 2) {
-    return 'You won! Rock beats Scissors'
+    return 'You won the round! Rock beats Scissors'
   } else if (player === 1 && computer === 0) {
-    return 'You won! Paper beats Rock'
+    return 'You won the round! Paper beats Rock'
   } else if (player === 1 && computer === 2) {
-    return 'You lost! Scissors beats Paper'
+    return 'You lost the round! Scissors beats Paper'
   } else if (player === 2 && computer === 0) {
-    return 'You lost! Rock beats Scissors'
+    return 'You lost the round! Rock beats Scissors'
   } else if (player === 2 && computer === 1) {
-    return 'You won! Scissors beats Paper'
+    return 'You won the round! Scissors beats Paper'
   } else {
     return 'Tie! No one wins'
   }
